970e0f2e9a14a4b4c8880094c0944175d86de192
KellyW-coder <79504971+KellyW-coder@users.noreply.github.com>
2021-05-25 19:22:42 +0200

    added axios, react loader spinner and imported files with updating the weather jscript

---
+++ src/Weather.js
@@ -0,0 +1,24 @@
+import React from "react";
+import axios from "axios";
+import Loader from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `Weather alert in ${response.data.name} is ${response.data.main.temp}°C`
+    );
+  }
+  let apiKey = "3ae6bdb32a731d95f7ea1efdd218128c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    <Loader
+      type="Puff"
+      color="#00BFFF"
+      height={100}
+      width={100}
+      timeout={3000} //3 secs
+    />
+  );
+}
